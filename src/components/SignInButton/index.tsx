160f21ffcdbcd16388879adf78a2bd2dde92b75f
Paulo Bezerra <bezerra.paulo@gmail.com>
2022-01-19 08:56:52 -0400

    completando o desafio do ignews

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import styles from "./styles.module.scss";
+import { FaGithub, FaSpinner } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/react";
+
+export function SignInButton() {
+  const { data, status } = useSession();
+
+  return status == "authenticated" ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" />
+      {data.user?.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#eba417" />
+      Sign in with GitHub
+    </button>
+  );
+}
